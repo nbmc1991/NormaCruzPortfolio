0768aafdd7c834108f53dbeaeb52795960cbb3af
Norma Cruz <nbmc1991@gmail.com>
2021-04-27 19:02:56 -0400

    testing

--- ncportfolio/src/components/Navbar.js
+++ ncportfolio/src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from "react";
 import logo from "../logo.png";
+//***REACT FONTAWESOME IMPORT */
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -10,7 +13,7 @@ const Navbar = () => {
 
   <a className="navbar-brand" href="/home"><img className="logo" src={logo} alt="nc logo"/></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <FontAwesomeIcon icon={faBars} style={{color: "#fff"}} />
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
