76e3bf78685e42c51bdc720180f317b57fb06ae5
Norma Cruz <nbmc1991@gmail.com>
2021-04-27 20:55:41 -0400

    header complete

---
+++ ncportfolio/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Norma Cruz</h1>
+                <Typed 
+                className="typed-text"
+                strings={["Full Stack Web Development", "Web Designer", "Social Media Designer"]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+                />
+                <a href="#" className="btn-main-offer">Contact me</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
