c2ac8996da4371330f342975a26be8912469fd5e
Norma Cruz <nbmc1991@gmail.com>
2021-05-20 14:30:05 -0400

    resume uploaded

--- ncportfolio/src/components/resume.js
+++ ncportfolio/src/components/resume.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import {Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton} from "react-mdl"
+import ncresume from "../ncresume1.png";
 
 
 class Resume extends Component {
     render (){
         return (
             <Card shadow={0} style={{width: '512px', margin: 'auto'}}>
-            <CardTitle style={{color: '#ff', height: '176px', background: 'url(https://github.com/nbmc1991/NormaCruzPortfolio/blob/master/ncportfolio/src/NCresume.png) center / cover'}}>Welcome</CardTitle>
+            <CardTitle style={{color: '#ff', height: '176px', background:{ncresume} }}>Welcome</CardTitle>
             <CardText>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Mauris sagittis pellentesque lacus eleifend lacinia...
